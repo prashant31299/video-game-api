cdb36c2e75a1a5d53257e849323b72f3911bd5cb
prashantchavan <prashantchavan312199@gmail.com>
2022-03-31 01:05:57 +0530

    game api" "

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+ const [gameTitle,setGameTitle]=useState(' ')
+ const [serchedgames,setserchgames] =useState('')
+console.log(serchedgames);
+
+  const searchGame= async()=>{
+  const responce= await fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
+  const data= await responce.json()
+  setserchgames(data)
+  // console.log(data)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+      
+<div className='searchSection'>
+  <h1>search for A game </h1>
+  <input 
+  type='text' 
+  placeholder='MineCraft ..... '
+  onChange={(event)=>{
+    setGameTitle(event.target.value)
+  }}
+  />
+  <button onClick={searchGame}>search game </button>
+  <div className='games'>
+  {serchedgames.map((game,key)=>{
+    return(
+      <div className='game'>{game.external}</div>
+    )
+  })}
+  </div>
+  
+  
+  
+  </div>      
+      <div className='dealSeaction'>
+        <h1>Latest Deals </h1>
+      </div>
+     </div>
   );
 }
 
